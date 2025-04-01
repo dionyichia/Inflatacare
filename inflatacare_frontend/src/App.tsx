b21d6a87384205e2cde20cf47f9c0fcdd847051e
dionyichia <dionyi2002@gmail.com>
2025-04-01 11:41:02 +0800

    Added arduino iot node mod

--- inflatacare_frontend/src/App.tsx
+++ inflatacare_frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='flex center justify-center'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="w-0.5">
+      <p className="w-full text-lg">
         Click on the Vite and React logos to learn more
       </p>
     </>
